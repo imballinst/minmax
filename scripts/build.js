10a2d131235394eb5dfb5c096dbdf04b3c262542
Try Ajitiono <ballinst@gmail.com>
2021-11-14 10:15:25 +0700

    fix build script
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/build.js
+++ scripts/build.js
@@ -4,9 +4,11 @@ const execa = require('execa');
 const { ROOT_FOLDER, SRC_FOLDER, DIST_FOLDER } = require('./constants');
 const { doesPathExist } = require('./common/exists');
 
+const PATH_TO_DIST_INDEX_HTML = `${DIST_FOLDER}/index.html`;
+
 // Copy all stuff in `src` folder, except `src/dev`.
 // Later, the `dist/css/tailwind.css` will be replaced with the purged version, too.
-(async () => {
+async function main() {
   if (await doesPathExist(DIST_FOLDER)) {
     await fs.rm(DIST_FOLDER, { recursive: true });
   } else {
@@ -21,6 +23,32 @@ const { doesPathExist } = require('./common/exists');
     }
   });
 
-  // Generate the tailwind.
+  // Generate the minified Tailwind CSS.
   await execa.command('yarn css:build', { cwd: ROOT_FOLDER });
-})();
+
+  // Remove the dev scripts from index.html.
+  const content = await fs.readFile(PATH_TO_DIST_INDEX_HTML, 'utf-8');
+  const contentArray = content.split('\n');
+  const cleanedHtmlArray = [];
+  let isLineIncluded = true;
+
+  for (const line of contentArray) {
+    const trimmed = line.trim();
+
+    if (trimmed.startsWith('<!-- [remove-on-prod:start')) {
+      isLineIncluded = false;
+    }
+
+    if (isLineIncluded) {
+      cleanedHtmlArray.push(line);
+    }
+
+    if (trimmed.startsWith('<!-- [remove-on-prod:finish')) {
+      isLineIncluded = true;
+    }
+  }
+
+  await fs.writeFile(PATH_TO_DIST_INDEX_HTML, cleanedHtmlArray.join('\n'));
+}
+
+main();
